a03abbfabdbb8758ee51db75d355bc9ace582849
Hanxuehua1013 <hanxuehua_1013@163.com>
2018-12-30 21:31:27 +0800

    aaa

--- src/main/java/com/itheima/dubboxdemo/controller/UserController.java
+++ src/main/java/com/itheima/dubboxdemo/controller/UserController.java
@@ -10,18 +10,19 @@ import com.itheima.dubboxdemo.service.UserService;
 @Controller
 @RequestMapping("/user")
 public class UserController {
-	
+
 	@Reference
 	private UserService userService; // 远程注入用户服务
+
 	/**
 	 * 
-	* @Title: getName 
-	* @Description: TODO 
-	* @return String
+	 * @Title: getName
+	 * @Description: TODO
+	 * @return String
 	 */
 	@RequestMapping("/getName.do")
 	@ResponseBody
-	public String  getName() {
+	public String getName() {
 		return userService.getName();
 	}
 }
